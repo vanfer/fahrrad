38ae5512bef03e7a470cbd08d6e9ba165b765d21
vanfer <vanessa.ferrarello@gmail.com>
2016-11-21 21:35:32 +0100

    Views für den Zentralen Display überarbeitet: - fehlende Elemente erweitert (Tagesstatistik, Highscore, Batterie) - alle Elemente neu angeordnet und damit alles einheitlicher wird Wrapper durch Panels ersetzt und Graphen ebenfalls mit Panels "umrahmt" - Style.css wurde dementsprechend verändert und per Kommentare in inhaltliche Bereiche gegliedert, Eigenschaften alphabetisch sortiert

--- resources/views/central/index.blade.php
+++ resources/views/central/index.blade.php
@@ -1,19 +1,62 @@
 @extends("layouts.app")
 
 @section("content")
-    <div id="track-wrapper">
-        <canvas id="track"></canvas>
+
+    <div class ="container-fluid">
+        {{-- Anzeige Streckenmodus  --}}
+        <div class="panel panel-default pull-left strecken-panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Streckenmodus</h3>
+            </div>
+            <div class="panel-body">
+                <div id="track-wrapper col-lg-12">
+                    <canvas id="track"></canvas>
+                </div>
+            </div>
+        </div>
+
+        {{-- Anzeige der Leistung im Modus konstante Leistung und konstanter Drehmoment --}}
+        <div class="panel panel-default pull-left leistung-panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Leistung</h3>
+            </div>
+            <div class="panel-body">
+                <div id="energy-current-wrapper col-lg-12">
+                    <canvas id="energy-current"></canvas>
+                </div>
+            </div>
+        </div>
+
+        {{-- Anzeige Gesamtenergie  --}}
+        <div class="panel panel-default pull-left gesamtleistung-panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Gesamtleistung</h3>
+            </div>
+            <div class="panel-body">
+                <div id="energy-wrapper col-lg-12">
+                    <canvas id="energy"></canvas>
+                </div>
+            </div>
+        </div>
+
+        <div class="clear"></div>
     </div>
 
-    <div id="energy-current-wrapper">
-        <canvas id="energy-current"></canvas>
-    </div>
 
-    <div id="energy-wrapper">
-        <canvas id="energy"></canvas>
-    </div>
 
-    <div class="clear"></div>
-
+    {{-- Anzeige der Fahrerdetails  --}}
     @include("central.fahrrad-information")
+
+    <div class ="container-fluid">
+        {{-- Anzeige der Tagesstatistik --}}
+        @include("central.tagesstatistik")
+        {{-- Anzeige der Highscore  --}}
+        @include("central.highscore")
+        {{-- Anzeige der Batterieladung  --}}
+        @include("central.batterieladung")
+        <div class="clear"></div>
+    </div>
+
 @endsection
+
+
