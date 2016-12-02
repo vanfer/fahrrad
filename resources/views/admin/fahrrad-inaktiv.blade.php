bcb340b6950e027719bb44d4171ccf7e3844d787
Clara <claraterbeck@gmail.com>
2016-12-02 13:11:29 +0100

    Buttons geändert, Tabelle erstellt

--- resources/views/admin/fahrrad-inaktiv.blade.php
+++ resources/views/admin/fahrrad-inaktiv.blade.php
@@ -3,13 +3,13 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title pull-left">Fahrrad {{$fahrrad->id}}</h3>
-                <button type="button" class="btn btn-default pull-right ">
-                    <span class="glyphicon glyphicon-question-sign">Hilfe</span>
-                </button>
-                <button type="button" class="btn btn-default pull-right ">
-                    <span class="glyphicon glyphicon-plus"></span>
-                    Hinzufügen
-                </button>
+                <div class="btn-group pull-right" role="group">
+                    <button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-plus"></span>
+                        Zuordnung hinzufügen
+                    </button>
+                    <button type="button" class="btn btn-default">Hilfe</button>
+                </div>
                 <span class="clearfix"></span>
             </div>
             <div class="panel-body">
